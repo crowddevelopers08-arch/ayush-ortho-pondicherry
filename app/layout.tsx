8ff14f84be6fdf6fffdbade1a6cd32519fa7b43a
fenil matt fenil matt <ffenilmatt@gmail.com>
2026-04-07 15:23:33 +0530

    pes

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,6 +71,19 @@ export default function RootLayout({
             `,
           }}
         />
+
+        {/* Microsoft Clarity */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "w7wjr7yg42");
+            `,
+          }}
+        />
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
