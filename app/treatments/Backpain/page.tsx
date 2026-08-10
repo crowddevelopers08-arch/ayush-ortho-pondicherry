8abb48bb890826df81c7c8fe131a62ee586f0d92
fenil matt fenil matt <ffenilmatt@gmail.com>
2026-08-10 16:04:35 +0530

    chanlshhssds

--- app/treatments/Backpain/page.tsx
+++ app/treatments/Backpain/page.tsx
@@ -14,6 +14,21 @@ import Script from "next/script"
 export default function HomePage() {
   return (
     <>
+      {/* Google Tag Manager - Head Script */}
+      <Script
+        id="gtm-head"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WH34B7DF');
+          `
+        }}
+      />
+
       {/* Google Analytics / Google Ads Scripts */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=AW-18044684782"
@@ -47,6 +62,16 @@ export default function HomePage() {
           }
         `}
       </Script>
+
+      {/* Google Tag Manager - Noscript (Body) */}
+      <noscript>
+        <iframe 
+          src="https://www.googletagmanager.com/ns.html?id=GTM-WH34B7DF"
+          height="0" 
+          width="0" 
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
 
       <main className="min-h-screen">
         <Header />
